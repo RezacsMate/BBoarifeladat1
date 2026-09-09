a7a4bbd44f4651a78db34dbf7a6ac674067f28dd
RezacsMate <Rezacs.Mate@C19-05>
2026-09-09 09:45:39 +0200

    as

--- BBoarifeladat1/Library.cs
+++ BBoarifeladat1/Library.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,11 @@ namespace BBoarifeladat1
         private List<Book> books = new List<Book>();
         public string Name { get; set; }
 
-
+        public Library(string name)
+        {
+            Name = name;
+            List<Book> books = new List<Book>();
+        }
+        public void AddBooks()
     }
 }
